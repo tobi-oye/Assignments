ec17feeba55b8f3bf2ab372a0811143681c019e2
tobi-oye <[tobiloba5000@gmail.com]>
2020-05-06 11:14:10 +0100

    correction to the assignment

--- second_assignment.py
+++ second_assignment.py
@@ -1,12 +1,12 @@
 # Fifth assignment:
 # Create a function that takes in an array of numbers. The function should multiply all elements of the array by 2 and return the modified array
-def new_func( elements = []):
+def array_multiplier_2 ( elements = []):
     new_array = [element * 2 for element in elements]
     return new_array
 # Sixth assignment:
 # Create a function that takes in 2 arguments. The first argument is an array and the second argument takes in a value. The function should push whatever value from the 2nd arguments into the array and return the modified array
 
-def new_array (elements,y):
+def append_second_arguement (elements,y):
     elements.append(y)
     return elements
 # Seventh assignment: (V. Hard)
@@ -35,13 +35,13 @@ def new_array (elements,y):
 data = [{'principal': 2500, 'time':1.8},{'principal': 1500, 'time':5},{'principal':3000, 'time':1},\
     {'principal': 2000, 'time':3}]
 # create a function that takes an array as an argument
-def interestCalculator(x = []):
+def interestCalculator(elements = []):
     interest_data = []
     # iterate through the elements in the list in order to get the values of principal and time
     i = 0 
-    for element in x:
+    for element in elements:
         # * If the principal is greater than or equal to 2500 and the time is greater than 1 and less than 3, then rate = 3
-        if element['principal'] >= 2500 and element['time'] < 3:
+        if element['principal'] >= 2500 and element['time'] > 1 and element['time'] < 3:
             # set the rate equal to 3 
             rate = 3
         # * If the principal is greater than or equal to 2500 and the time is greater than or equal to 3, then rate = 4
@@ -52,10 +52,11 @@ def interestCalculator(x = []):
             rate = 2
         # * Otherwise, rate = 1;
         else:
-            rate = 1
+            rate = 1 
         interest = (element['principal'] * rate * element['time'])/100
-        interest_data.append(interest)
-        i += 1
+
+        interest_data.append({'principal':element['principal'],'time':element['time'],'interest':interest})
+       
     return interest_data
 
 interestCalculator(data)
